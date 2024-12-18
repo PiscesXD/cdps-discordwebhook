d6f723673c7be4d12d346d93ee9e6774e8283824
PiscesXD <88549470+PiscesXD@users.noreply.github.com>
2024-12-19 04:55:53 +0800

    1.1.0
    新增 : 許多功能

--- example/main.py
+++ example/main.py
@@ -1,10 +1,14 @@
 import requests
 import json
-import time
-from datetime import datetime 
+import os
+from datetime import datetime
 
 class DiscordWebhook:
-    def __init__(self, WebhookUrl = None):
+    def __init__(self, WebhookUrl=None):
+        """
+        如果是單純生成Embed訊息，可以不用填入
+        WebhookUrl : 要填入Webhook網址
+        """
         self.WebhookUrl = WebhookUrl
         self.colors = {
             "red": 0xFF0000,
@@ -13,52 +17,146 @@ class DiscordWebhook:
             "white": 0xFDFFFF,
             "black": 0x000000
         }
-        self.content = None
+        self.content = ""
         self.avatar_url = None
         self.username = None
-        self.embed = {"fields": [] , "footer" : {} , "author" : {}}
+        self.embed = {}
+        self.file_paths = []
+        self.image_path = {"image" : None}
+        self.message_id = None
 
-    def __send_webhook__(self):
-        headers = {
-            "Content-Type": "application/json"
+    def send_webhook__(self , arg = None):
+        files = {}
+        data = {
+            "username": self.username,
+            "avatar_url": self.avatar_url,
         }
-
-        senddata = {"username" : self.username,
-                   "avatar_url" : self.avatar_url,
-                   "embeds": [self.embed]}
         
         if self.content:
-            senddata["content"] = self.content
+            data["content"] = self.content
+            
+        embeds = []
+        if isinstance(arg, list):
+            embeds = arg
+        elif self.embed:
+            embeds = [self.embed]
 
-        response = requests.post(self.WebhookUrl, headers=headers, data=json.dumps(senddata))
+        if not data.get("content") and not embeds:
+            raise ValueError("無法發送空消息，請設置content或有效的embeds")
+
+        for embed in embeds:
+            if "image_path" in embed:
+                image_path = embed["image_path"]
+                if os.path.isfile(image_path):
+                    file_name = os.path.basename(image_path)
+                    img_file = open(image_path, "rb")
+                    files[file_name] = (file_name, img_file, "image/jpeg")
+                    embed["image"] = {"url": f"attachment://{file_name}"}
+                del embed["image_path"]
+
+            if not isinstance(arg, list) and self.image_path['image']:
+                if os.path.isfile(self.image_path['image']):
+                    file_name = os.path.basename(self.image_path['image'])
+                    img_file = open(self.image_path['image'], "rb")
+                    files[file_name] = (file_name, img_file, "image/jpeg")
+                    embed["image"] = {"url": f"attachment://{file_name}"}
+
+        if self.file_paths:
+            for file_path in self.file_paths:
+                file_name = os.path.basename(file_path)
+                file = open(file_path, "rb")
+                files[file_name] = (file_name, file, "application/octet-stream")
+
+        data["embeds"] = embeds
         
-        if response.status_code == 204:
-            return {"code" : response.status_code , 'message' : 'done'}
+        response = requests.post(self.WebhookUrl+"?wait=true", data={"payload_json": json.dumps(data)}, files=files)
+
+        for file in files.values():
+            file[1].close()
+
+        if response.status_code in [200, 204]:
+            message_id = None
+            if response.status_code == 200:
+                try:
+                    message_id = response.json().get('id')
+                except ValueError:
+                    pass
+            return {"code": response.status_code, 'message': 'done', 'message_id': message_id}
         else:
-            return {"code" : response.status_code , 'message' : {response.text}}
-    
-    def dataembed(self):
-        return self.embed
+            return {"code": response.status_code, 'message': response.text}
 
     def set_content(self, message):
+        """
+        功能 : 設置一般訊息
+        message : 訊息
+        """
         self.content = message
 
-    def set_image(self, url):
-        self.embed["image"] = {"url": url}
+    def set_image(self, image):
+        """
+        功能 : 嵌入訊息_設置照片
+        image : 檔案路徑或網址
+        """
+        if isinstance(image, str):
+            if image.startswith("http"):
+                self.embed["image"] = {"url": image}
+            elif os.path.isfile(image):
+                self.image_path['image'] = image 
+                file_name = os.path.basename(image)
+                self.embed["image"] = {"url": f"attachment://{file_name}"}
+            else:
+                raise ValueError("無效的圖片路徑或網址")
+        else:
+            raise TypeError("圖片參數必須是字串，表示本地路徑或網址")
+
+    def set_files(self, file_paths):
+        """
+        功能 : 發送檔案
+        file_paths : 檔案路徑
+        """
+        if isinstance(file_paths, list):
+            for file_path in file_paths:
+                if not os.path.isfile(file_path):
+                    raise ValueError(f"檔案不存在: {file_path}")
+            self.file_paths = file_paths
+        else:
+            raise TypeError("檔案參數必須是列表")
 
     def set_title(self, title):
+        """
+        功能 : 嵌入訊息_設置標題
+        title : 訊息
+        """
         self.embed["title"] = title
 
     def set_description(self, description):
+        """
+        功能 : 嵌入訊息_設置描述
+        description : 訊息
+        """
         self.embed["description"] = description
 
     def set_avatar(self, avatar_url):
+        """
+        功能 : 設置使用者頭貼
+        avatar_url : 網址
+        """
         self.avatar_url = avatar_url
 
     def set_username(self, username):
+        """
+        功能 : 設置使用者名稱
+        username : 名字
+        """
         self.username = username
 
     def set_color(self, color):
+        """
+        功能 : 嵌入訊息_設置顏色
+        color : 名稱
+        value : 訊息
+        inline : 是否在同一行，但要使用bool
+        """
         if color in self.colors:
             self.embed["color"] = self.colors[color]
         elif isinstance(color, str) and color.startswith('0x'):
@@ -78,52 +176,165 @@ class DiscordWebhook:
         else:
             raise TypeError("顏色無效請用16進位字串或整數")
 
-    def set_value(self, name, value, inline=False):
-        self.embed["fields"].append({"name" : name , "value" : value , "inline" : inline})
+    def set_value(self, name, value, inline=True):
+        """
+        功能 : 嵌入訊息_設置value
+        name : 名稱
+        value : 訊息
+        inline : 是否在同一行，但要使用bool值
+        """
+        if "fields" not in self.embed.keys():
+            self.embed["fields"] = []
+        self.embed["fields"].append({"name": name, "value": value, "inline": inline})
 
     def set_footer(self, text, icon_url=None):
+        """
+        功能 : 嵌入訊息_設置頁尾
+        text : 資訊
+        icon_url : 填入網址
+        """
+        if "footer" not in self.embed.keys():
+            self.embed["fields"] = {}
         self.embed["footer"]["text"] = text
         if icon_url:
             self.embed["footer"]["icon_url"] = icon_url
 
     def set_author(self, name, icon_url=None):
+        """
+        功能 : 嵌入訊息_設置作者訊息
+        name : 名稱
+        icon_url : 填入網址
+        """
+        if "author" not in self.embed.keys():
+            self.embed["author"] = {}
         self.embed["author"]["name"] = name
         if icon_url:
             self.embed["author"]["icon_url"] = icon_url
 
     def set_thumbnail(self, url):
+        """
+        功能 : 嵌入訊息_設置縮圖
+        url : 填入網址
+        """
         self.embed["thumbnail"] = {"url": url}
 
     def set_timestamp(self, timestamp=None):
+        """
+        功能 : 嵌入訊息_設置時間戳
+        timestamp : 填入時間戳 或 不填會給現在時間
+        """
         if timestamp is not None:
             self.embed["timestamp"] = timestamp.isoformat()
         else:
             self.embed["timestamp"] = datetime.utcnow().isoformat()
 
-    def send(self):
-        return self.__send_webhook__()
+    def send(self, arg = None):
+        """
+        功能 : 發送訊息
+        arg : 可以發送多則embed訊息，但需使用 [] list 
+        """
+        return self.send_webhook__(arg)
+    
+    def dataembed(self):
+        """
+        功能 : 獲取Embed資訊
+        """
+        data = self.embed.copy()
+        if self.image_path['image']:
+            data["image_path"] = self.image_path['image']
+        return data
 
-    def embedssend(self , lists):
-        if not isinstance(lists, list):
-            raise TypeError("傳入參數型態必須是List")
-
-        if (len(lists) < 11):
-            raise ValueError("傳入的List數量不能超過10")
-
-        headers = {
-            "Content-Type": "application/json"
+    def delete(self, message_id):
+        """
+        功能 : 刪除訊息
+        message_id : 訊息ID
+        """
+        url = f"{self.WebhookUrl}/messages/{message_id}"
+        response = requests.delete(url)
+        if response.status_code == 204:
+            return {"message" : 'done'}
+        else:
+            return {"code" : response.status_code , "message" : response.text}
+        
+    def edit(self, message_id, new_content=None, new_embeds=None):
+        """
+        功能 : 編輯訊息
+        message_id : 訊息ID
+        new_content : 文字訊息
+        new_embeds : 嵌入訊息，如果多則請記得使用 dataembed()調用資料 包成[]
+        """
+        url = f"{self.WebhookUrl}/messages/{message_id}"
+        
+        files = {}
+        data = {
+            "username": self.username,
+            "avatar_url": self.avatar_url,
         }
-
-        senddata = {"username" : self.username,
-                   "avatar_url" : self.avatar_url,
-                   "embeds": lists}
         
         if self.content:
-            senddata["content"] = self.content
+            data["content"] = self.content
+            
+        embeds = []
+        if new_content:
+            data["content"] = new_content
 
-        response = requests.post(self.WebhookUrl, headers=headers, data=json.dumps(senddata))
-        
-        if response.status_code == 204:
-            return {"code" : response.status_code , 'message' : 'done'}
+        if isinstance(new_embeds, list):
+            embeds = new_embeds
+
+        if not data.get("content") and not embeds:
+            raise ValueError("無法發送空消息，請設置content或有效的embeds")
+
+        for embed in embeds:
+            if "image_path" in embed:
+                image_path = embed["image_path"]
+                if os.path.isfile(image_path):
+                    file_name = os.path.basename(image_path)
+                    img_file = open(image_path, "rb")
+                    files[file_name] = (file_name, img_file, "image/jpeg")
+                    embed["image"] = {"url": f"attachment://{file_name}"}
+                del embed["image_path"]
+
+            if not isinstance(new_embeds, list) and self.image_path['image']:
+                if os.path.isfile(self.image_path['image']):
+                    file_name = os.path.basename(self.image_path['image'])
+                    img_file = open(self.image_path['image'], "rb")
+                    files[file_name] = (file_name, img_file, "image/jpeg")
+                    embed["image"] = {"url": f"attachment://{file_name}"}
+
+        if self.file_paths:
+            for file_path in self.file_paths:
+                file_name = os.path.basename(file_path)
+                file = open(file_path, "rb")
+                files[file_name] = (file_name, file, "application/octet-stream")
+
+        data["embeds"] = embeds
+
+        response = requests.patch(url, data={"payload_json": json.dumps(data)}, files=files)
+
+        for file in files.values():
+            file[1].close()
+
+        if response.status_code in [200, 204]:
+            message_id = None
+            if response.status_code == 200:
+                try:
+                    message_id = response.json().get('id')
+                except ValueError:
+                    pass
+            return {"code": response.status_code, 'message': 'done', 'message_id': message_id}
         else:
-            return {"code" : response.status_code , 'message' : {response.text}}
+            return {"code": response.status_code, 'message': response.text}
+    
+
+    def set_clear(self):
+        """
+        恢復初始狀態
+        """
+        self.content = ""
+        self.avatar_url = None
+        self.username = None
+        self.embed = {}
+        self.file_paths = []
+        self.image_path = {"image" : None}
+        self.message_id = None
+        return {'message' : 'done'}
--- read.md
+++ read.md
@@ -15,90 +15,107 @@ DiscordWebhook(填入Discord WebHook網址)
 
 設定一般訊息
 ```py
-set_content("") 
+set_content("") < 文字 
 ```
 
-設定title
+設置圖片
 ```py
-set_title("")
+set_image("") < 填入網址或檔案路徑
 ```
 
-設定description
+設置檔案
 ```py
-set_description("")
+set_files("") < 檔案路徑
 ```
 
-設定color
+設置使用者頭貼
 ```py
-set_color("")
+set_avatar("") < 網址
 ```
 
-設定image
+設置使用者名稱
 ```py
-set_image("") < 填入圖片網址
+set_username("") < 文字
 ```
 
-設定avatar
+設置嵌入訊息 title
 ```py
-set_avatar("") < 填入圖片網址
+set_title("") < 文字
 ```
 
-設定username
+設置嵌入訊息 description
 ```py
-set_username("")
+set_description("") < 文字
 ```
 
-設定value
+設置嵌入訊息 顏色
 ```py
-set_value(name="" , value="" , inline=bool) < bool預設False
+set_color("") < 設置顏色
 ```
 
-設定footer
+設置嵌入訊息 value
 ```py
-set_footer(test="",icon_url="") < icon_url 圖片網址
+set_value("name" , "value" , inline=True) < inline 一定要填入bool
 ```
 
-設定author
+設置嵌入訊息 footer
 ```py
-set_author(test="",icon_url="") < icon_url 圖片網址
+set_footer("test", icon_url=None) < icon_url可不用填寫，但如要填寫請設置網址
 ```
 
-設定thumbnail
+設置嵌入訊息 author
 ```py
-set_thumbnail("")< 填入圖片網址
+set_author("name" , icon_url=None) < icon_url可不用填寫，但如要填寫請設置網址
 ```
 
-設定timestamp
+設置嵌入訊息 thumbnail
 ```py
-set_timestamp() < 可填寫 也可不填寫 請記住要使用utc來表示
+set_thumbnail("") < 填入網址
 ```
 
-發送訊息
+設置嵌入訊息 時間
 ```py
-send()
+set_timestamp("") < 填入時間戳 或 不填會給現在時間
 ```
 
-獲取Embed資料 < 用於發送多則embed
+獲取embed資料 < 用於發送多則embed訊息
 ```py
 dataembed()
 ```
 
-發送多則embed
+發送webhook訊息
 ```py
-embedssend([])
+send() < 如果要傳多則embed 請使用[] 可參考下方範例
+```
+
+刪除訊息
+```py
+delete('message_id') < 只能刪除指定的webhook發送過的訊息
+```
+
+編輯訊息
+```py
+edit('message_id' , 'new_content' , [new_embeds]) < 只能更改指定的webhook發送過的訊息
+預設 new_content 和 new_embeds 為 None
+```
+
+清楚所有設定
+```py
+set_clear() < 會回傳 {'message' : 'done'}
 ```
 
 回傳值只有 send 和 embedssend 有格式為
 ```py
-{"code" : code , "message" : message} 
+{"code" : code , "message" : message , message_id : message_id} 
 ```
+
 
 ## 範例1
 
 # 發送單則embed
 
 ```py
-webhook_url = ""
+webhook_url = "" < 填入 webhook網址
 webhook = DiscordWebhook(webhook_url)
 webhook.set_content("不好")
 webhook.set_title("你好")
@@ -114,15 +131,33 @@ webhook.send()
 
 # 發送多則embed
 ```py
-webhook_url = ""
+webhook_url = "" < 填入 webhook網址
 webhook = DiscordWebhook(webhook_url)
 webhook.set_title("你好")
-
+webhook.set_files(['awa.txt' , 'AAA.png'])
 webhook_data = webhook.dataembed()
 
 webhook2 = DiscordWebhook()
 webhook2.set_title("不好")
-webhook_data2 = webhook.dataembed()
+webhook2.set_image("www.png")
+webhook_data2 = webhook2.dataembed()
 
-webhook.embedssend([a , b])
+webhook.send([webhook_data , webhook_data2])
+```
+
+## 範例三
+
+# 編輯訊息
+```py
+webhook_url = "" < 填入 webhook網址
+webhook = DiscordWebhook(webhook_url)
+webhook.set_title("不好")
+webhook.edit('訊息ID',new_embeds=[webhook.dataembed()])
+```
+
+# 刪除訊息
+```py
+webhook_url = "" < 填入 webhook網址
+webhook = DiscordWebhook(webhook_url)
+webhook.delete('訊息ID')
 ```
